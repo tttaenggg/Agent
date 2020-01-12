1a316ac7ebfc9917f2fb2dd3178c5b1a97430cd9
soulweed <nontachai.y@gmail.com>
2020-01-12 14:24:52 +0700

    Complete UI Agent Flow

--- UIAgent/uiagent/agent.py
+++ UIAgent/uiagent/agent.py
@@ -21,12 +21,11 @@ DEFAULT_MESSAGE = 'I am a UI Agent'
 DEFAULT_AGENTID = "uiagent"
 DEFAULT_HEARTBEAT_PERIOD = 5
 
-
+# class FrontEndClass():
 class Uiagent(Agent):
     """
     Document agent constructor here.
     """
-
     def __init__(self, config_path,
                  **kwargs):
         super().__init__(**kwargs)
@@ -41,89 +40,180 @@ class Uiagent(Agent):
             self._logfn = _log.debug
         else:
             self._logfn = _log.info
-            
         
-
-    @Core.receiver("onstart")
-    def onstart(self, sender, **kwargs):
-        top = tkinter.Tk()
+    def build_ui(self):
+        
+        self.new_state = None
+        self.old_state = None
+        
+        self._mode = None
+        self.temperature = 25 # It Default Temperature.
+        self._control = None
+        self._temp = None
+        self.control_variable = None
+        self.temp_variable = None
+        self.mode_variable = None
+        self.top = tkinter.Tk()
         # Code to add widgets will go here...
-
-        top.geometry("480x320")
+        self.top.geometry("480x320")
         
-        def ecoCallBack():
-            # TODO : Send Command Here
-            pass
+        self.mode_frame = tkinter.Frame(self.top)
+        self.mode_frame.pack()
+        self.mode_variable = IntVar()
+        self.mode_variable.set(1)
+        self.mode_variable = tkinter.StringVar(value="manual")
+    
+        self.eco_btn = tkinter.Radiobutton(self.mode_frame, text="ECO", variable=self.mode_variable, command=self.callBack_mode,
+                            indicatoron=False, value="eco", width=18, height=5)
+        self.eco_btn.grid(row=0, column=0)
+        self.dr_btn = tkinter.Radiobutton(self.mode_frame, text="DR", variable=self.mode_variable, command=self.callBack_mode,
+                            indicatoron=False, value="dr", width=18, height=5)
+        self.dr_btn.grid(row=2, column=0)
+        self.ma_btn = tkinter.Radiobutton(self.mode_frame, text="MANUAL", variable=self.mode_variable, command=self.callBack_mode,
+                            indicatoron=False, value="manual", width=18, height=5)
+        self.ma_btn.grid(row=4, column=0)
+        
+        self.mode_frame.place(x=15, y= 30)
+        
+        self.ok_btn = Button(self.top, text = "OK", command = self.doSendCommand, height = 4, width = 30)
+        self.ok_btn.place(x = 185,y = 220)
+        
+    
+        self.switch_frame = tkinter.Frame(self.top)
+        self.switch_frame.pack()
 
-        def drCallBack():
-            # TODO : Send Command Here
-            pass
-
-        def doSendCommand():
-            print(switch_variable.get())
-
-        eco_btn = Button(top, text = "ECO MODE", command = ecoCallBack, height = 5, width = 15)
-        eco_btn.grid(row=2, column=0)
-        eco_btn.place(x = 10,y = 30)
-
-        dr_btn = Button(top, text = "DR MODE", command = drCallBack, height = 5, width = 15)
-        dr_btn.grid(row=2, column=0)
-        dr_btn.place(x = 10,y = 150)
-
-        ok_btn = Button(top, text = "OK", command = doSendCommand, height = 5, width = 30)
-        ok_btn.grid(row=2, column=0)
-        ok_btn.place(x = 180,y = 150)
-
-        switch_frame = tkinter.Frame(top)
-        switch_frame.pack()
-
-        switch_variable = IntVar()
-        switch_variable.set(1)
+        self.switch_variable = IntVar()
+        self.switch_variable.set(1)
         # selection = var.get()
 
-        switch_variable = tkinter.StringVar(value="medium")
+        self.switch_variable = tkinter.StringVar(value="medium")
+        self.aut_button = tkinter.Radiobutton(self.switch_frame, text="Auto", variable=self.switch_variable,
+                                    indicatoron=False, value="auto", width=8, height=2)
 
-        aut_button = tkinter.Radiobutton(switch_frame, text="Auto", variable=switch_variable,
-                                    indicatoron=False, value="auto", width=8)
-        low_button = tkinter.Radiobutton(switch_frame, text="Low", variable=switch_variable,
-                                    indicatoron=False, value="low", width=8)
-        med_button = tkinter.Radiobutton(switch_frame, text="Medium", variable=switch_variable,
-                                    indicatoron=False, value="medium", width=8)
-        high_button = tkinter.Radiobutton(switch_frame, text="High", variable=switch_variable,
-                                    indicatoron=False, value="high", width=8)
+        self.low_button = tkinter.Radiobutton(self.switch_frame, text="Low", variable=self.switch_variable,
+                                    indicatoron=False, value="low", width=8, height=2)
 
-        aut_button.pack(side="left")
-        low_button.pack(side="left")
-        med_button.pack(side="left")
-        high_button.pack(side="left")
+        self.med_button = tkinter.Radiobutton(self.switch_frame, text="Medium", variable=self.switch_variable,
+                                    indicatoron=False, value="medium", width=8, height=2)
 
-        switch_frame.place(x=180, y=30)
+        self.high_button = tkinter.Radiobutton(self.switch_frame, text="High", variable=self.switch_variable,
+                                    indicatoron=False, value="high", width=8, height=2)
 
+        self.aut_button.pack(side="left")
+        self.low_button.pack(side="left")
+        self.med_button.pack(side="left")
+        self.high_button.pack(side="left")
 
+        self.switch_frame.place(x=185, y=30)
 
-        if switch_variable.get() == 'auto':
-            print('auto')
-        elif switch_variable.get() =='low':
-            print('low')
-        elif switch_variable.get() =='medium':
-            print('medium')
-        elif switch_variable.get() == 'high':
-            print('high')
+        # -- UI Toggle Button group 2 --------------------------
+        # ------------------------------------------------------
+        self.control_frame = tkinter.Frame(self.top)
+        self.control_frame.pack()
+        self.control_variable = IntVar()
+        # self.control_variable.set(1)
+        # self.control_variable = tkinter.StringVar(value="on")
+        self.on_button = tkinter.Radiobutton(self.control_frame, text="ON", variable=self.control_variable,
+                                    indicatoron=False, value=1, width=8, height=2, command=self.callBack_control)
+
+        self.off_button = tkinter.Radiobutton(self.control_frame, text="OFF", variable=self.control_variable,
+                                    indicatoron=False, value=0, width=8, height=2, command=self.callBack_control)
+
+        self.on_button.pack(side="left")
+        self.off_button.pack(side="left")
+        self.control_frame.place(x=185, y=80)
+
+        # -- UI UP DOWN -----------------------------------
+        # -------------------------------------------------
+        self.temp_frame = tkinter.Frame(self.top)
+        self.temp_frame.pack()
+        self.temp_variable = IntVar()
+        self.up_down_frame = tkinter.Frame(self.top)
+        self.up_down_frame.pack()
+        self.up_button = tkinter.Button(self.up_down_frame, 
+                                            font=('calibri', 12),
+                                            text="UP", 
+                                            width=4, height=1,
+                                            command=self.callBack_up)
+
+        self.down_button = tkinter.Button(self.up_down_frame,
+                                            font=('calibri', 12),
+                                            text="DOWN",
+                                            width=4, height=1,
+                                            command=self.callBack_down)
+    
+        self.up_button.pack(side="left")
+        self.down_button.pack(side="left")
+        
+        self.up_down_frame.place(x=325,y=83)
+        self.temp_frame.place(x=325, y=80)
+        
+        # -- Label Temperature Widget.
+        self.lbl = Label(self.top, font = ('calibri', 40, 'bold'), 
+                            background = 'whitesmoke', 
+                            foreground = 'springgreen2', text= str(float(self.temperature)),
+                            width=7, height=1, justify='center'
+                            ) 
+    
+        self.lbl.place(x=188, y=140)
+        self.top.mainloop()
+    
+    def callBack_mode(self):
+        self._mode = self.mode_variable.get()
+        if self._mode != 'manual':
+            self.ok_btn['state'] = DISABLED
+            if self._mode == 'dr': # DR Mode for Enable Trigger Signal from Outside.
+                print("DR Mode Actived")
+                # TODO : Send msg to Agent Optimizer Here.
+                
+            elif self._mode == 'eco': # ECO Mode is Automation adjust Temperature.
+                print("ECO Mode Activated")
+                # TODO : Send msg to Agent Optimizer Here.
         else:
-            print('Can not get val')
-
-        top.mainloop()
+            self.ok_btn['state'] = NORMAL
+            print("MANUAL Mode Activated")
+            # TODO : Send msg to Agent Optimizer for stop all automatic task and wait command from USER Only.
+    
+    def callBack_control(self):
+        self._control = self.control_variable.get()
+    
+    def callBack_up(self):
+        # self._temp = self.temp_variable.get()
+        self.temperature = self.temperature + 1
+        self.lbl.config(text=str(float(self.temperature)))
+        
+    def callBack_down(self):
+        # self._temp = self.temp_variable.get()
+        self.temperature = self.temperature - 1
+        self.lbl.config(text=str(float(self.temperature)))
+    
+    def doSendCommand(self):
+        print(self.switch_variable.get())
+        # print(self.control_variable.get())
+        # print(self.temp_variable.get())
+        if self._control:
+            self._control = "ON"
+        else:
+            self._control = "OFF"
+        print({'CONTROL': self._control, "SETPOINT": self.temperature, 
+                    "FANLEVEL": self.switch_variable.get()})
+        
+                
+    @Core.receiver("onstart")
+    def onstart(self, sender, **kwargs):
+        self._ui = self.build_ui()
         
         
-
+        
     @Core.receiver("onstop")
     def onstop(self, sender, **kwargs):
         """
         This method is called when the Agent is about to shutdown, but before it disconnects from
         the message bus.
         """
-        pass
-
+        self._ui.top.quit()
+    
+    
     @RPC.export
     def rpc_method(self, arg1, arg2, kwarg1=None, kwarg2=None):
         """
@@ -131,6 +221,8 @@ class Uiagent(Agent):
 
         May be called from another agent via self.core.rpc.call """
         return self.setting1 + arg1 - arg2
+
+
 
 def main():
     """Main method called to start the agent."""
